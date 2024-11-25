ae531b94baee2e8049c6bc7053d1fb3ead8176cd
Ayo <talktojmcvibes@gmail.com>
2024-11-25 13:09:35 +0100

    footer element

---
+++ src/app/footer.tsx
@@ -0,0 +1,13 @@
+
+
+const footer = () => {
+  return (
+    <div className="w-[100%] h-[40px] bg-[#1abc9c] text-center">
+      <p className="text-[16px] text-[#FFF]">
+        Made with ❤️ by AyDesigns
+      </p>
+    </div>
+  )
+}
+
+export default footer
