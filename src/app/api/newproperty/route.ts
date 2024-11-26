1c7f3ee0c28e439632f4af3c5c19b6dd19648fa5
Ayo <talktojmcvibes@gmail.com>
2024-11-26 14:40:44 +0100

    add new property API

---
+++ src/app/api/newproperty/route.ts
@@ -0,0 +1,29 @@
+import { connectDb } from "@/lib/connectDb";
+import propertyModels from "@/models/props";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request){
+    try {
+        await connectDb();
+        const data = await request.json()
+        const {tag, name, size, shortDescription, amount, description, location, imageURL} = data;
+        if(!tag || !name || !size || !shortDescription || !amount || !description || !location || !imageURL){
+            return NextResponse.json({message: "one or more entries are empty"}, {status: 400})
+        }
+        const newImageUrl = imageURL.split(',').map((url: string) => url.trim())
+        const newData = new propertyModels({
+            tag,
+            name,
+            shortDescription,
+            amount,
+            location,
+            description,
+            imageURL: newImageUrl,
+        })
+        await newData.save();
+        return NextResponse.json({message: "New property added"}, {status: 200})
+    } catch (err) {
+        console.log('Error adding new property', err)
+        return NextResponse.json({message: "Internal server error"}, {status: 500})
+    }
+}
