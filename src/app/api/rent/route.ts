34c8e40d22fce8d401f805cb6353e3136f3797d1
Ayo <talktojmcvibes@gmail.com>
2024-11-26 12:46:29 +0100

    rent data API

---
+++ src/app/api/rent/route.ts
@@ -0,0 +1,17 @@
+import propertyModels from "@/models/props";
+import { connectDb } from "@/lib/connectDb";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    try {
+        await connectDb();
+        const rentdata = await propertyModels.find({tag: "rent"});
+        if(!rentdata){
+            return NextResponse.json([], {status: 201})
+        }
+        return NextResponse.json(rentdata, {status: 200})
+    } catch (err) {
+        console.log("Error getting lease data", err);
+        return NextResponse.json({message: "Internal server error"}, {status: 500})
+    }
+}
