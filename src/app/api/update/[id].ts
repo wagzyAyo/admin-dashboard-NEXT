9b7ebd5d906a9d0836cb40c675b821f3519d1ba5
Ayo <talktojmcvibes@gmail.com>
2024-11-26 13:20:26 +0100

    update property by ID API

---
+++ src/app/api/update/[id].ts
@@ -0,0 +1,22 @@
+import { connectDb } from "@/lib/connectDb";
+import propertyModels from "@/models/props";
+import { NextResponse } from "next/server";
+
+export async function PUT(request: Request, {params}: {params: {id: string}}){
+    try {
+        await connectDb();
+        const {id} = params;
+        const newData = await request.json()
+        const updateProperty = await propertyModels.findOneAndUpdate({_id: id}, newData, {
+            new: true
+        });
+
+        if(!updateProperty){
+            return NextResponse.json({message: "No property found with the Id"}, {status: 400})
+        }
+        return NextResponse.json({message: "Property updated!"}, {status: 200})
+    } catch (err) {
+        console.log('Error updating data', err);
+        return NextResponse.json({message: "Internal server error"}, {status: 500})
+    }
+}
