b7d42ac0aab7fb090f3f5c19f2e33b7b0dae3f8a
Ayo <talktojmcvibes@gmail.com>
2024-11-26 12:34:03 +0100

    sales data api

---
+++ src/app/api/sales/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import { connectDb } from "@/lib/connectDb";
+import propertyModels from "@/models/props";
+
+export async function GET(){
+    await connectDb()
+    try {
+        
+        const salesData = await propertyModels.find({tag: 'sale'})
+        if(!salesData){
+            return NextResponse.json([],{status: 201})
+        }
+        return NextResponse.json(salesData, {status: 200})
+    } catch (err) {
+        console.log("Error getting sales data", err);
+        return NextResponse.json({message: "Internal server error"}, {status: 500})
+    }
+}
