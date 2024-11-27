9cfd0de47f5cb82ed6870a0a8ebf8a98fd0407f5
Ayo <talktojmcvibes@gmail.com>
2024-11-27 11:20:54 +0100

    update sale route

--- src/app/sale/page.tsx
+++ src/app/sale/page.tsx
@@ -1,9 +1,49 @@
+"use client"
+import { useState, useEffect } from "react"
+import Card from "@/components/card"
+import axios from "axios"
 
 
+interface propData {
+  _id: string,
+  name: string,
+  size: string,
+  shortDescription: string,
+  amount: number,
+  location: string,
+  description: string,
+  imageURL: string[]
+}
 const Page = () => {
+  const [data, setData] = useState<propData[]>([])
+
+  useEffect(()=>{
+    const getData = async ()=>{
+      try {
+        const response = await axios.get('/api/sales')
+        setData(response.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getData()
+   
+  }, [])
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Sale route</h1>
+      <h1 className="text-2xl">Sales</h1>
+         {data.map((propData)=>{
+          return <Card  
+          key={propData._id} 
+          name={propData.name} 
+          size={propData.size} 
+          amount={propData.amount} 
+          short={propData.shortDescription} 
+          location={propData.location} 
+          description={propData.description} 
+          imageURL={propData.imageURL}/>
+         })}
     </div>
   )
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,15 +1,25 @@
+interface cardProps{
+    name: string,
+    size: string,
+    location: string,
+    amount: number,
+    short: string,
+    description: string
+    imageURL: string[]
+}
 
-
-const Card = ({props}: {props: {name: string, size: string, amount: number, short: string, description: string, imageUrl: []}}) => {
-    const {name, size, amount, short, description} = props
+const Card = ({name, size, location,short, amount, description}: cardProps) => {
+    
   return (
     <div className="w-[300px] h-[300px] border-[none] rounded-[20px] px-[15px] py-[10px] bg-[#cbc4c4]">
       <div className="flex justify-start align-middle flex-wrap gap-x-[15px]">
         <div>Name: {name}</div>
         <div>Size: {size}</div>
+        <div>Location: {location}</div>
         <div>Amount: {amount}</div>
         <div>Short: {short}</div>
         <div>Description: {description}</div>
+        <div></div>
       </div>
     </div>
   )
