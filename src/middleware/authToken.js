8d07f498033055b05a753f4ae20b92af537b03d4
Ayo <talktojmcvibes@gmail.com>
2024-12-01 12:22:17 +0100

    authToken for user authentication

---
+++ src/middleware/authToken.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken'
+import userModel from "../models/user"
+
+const authToken = async (req, res, next)=>{
+    const token = req.cookies.jwt;
+
+    if (!token){
+        return res.status(400).json({message: "Access denied. You need authorization"})
+    }
+    try {
+      const decoded = jwt.verify(token, process.env.SECERET);
+      req.user = await userModel.findById(decoded.userId).select('-password');
+      next()
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json({message: "Invalid token"})
+    }
+}
+
+module.exports = authToken
