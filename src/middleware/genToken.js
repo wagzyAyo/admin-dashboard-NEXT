f393ec691da1decfe11a7b37873dc00225007ab1
Ayo <talktojmcvibes@gmail.com>
2024-12-02 08:28:23 +0100

    update genToken

--- src/middleware/genToken.js
+++ src/middleware/genToken.js
@@ -1,10 +1,12 @@
 import jwt from 'jsonwebtoken'
 
 const generateToken = (res, userId) =>{
-    const token = jwt.sign({userId}, process.env.SECERET);
+    const token = jwt.sign({userId}, process.env.SECERET,{expiresIn: "30d"});
 
     res.cookie('jwt', token, {
         httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
         maxAge:  30 * 24 * 60 * 60 * 1000
     });
 }
