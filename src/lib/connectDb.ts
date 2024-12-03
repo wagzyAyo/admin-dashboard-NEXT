2aa62e34a17b3fd47c95869090b05538fee207bf
Ayo <talktojmcvibes@gmail.com>
2024-12-03 10:47:32 +0100

    update db connection string

--- src/lib/connectDb.ts
+++ src/lib/connectDb.ts
@@ -4,13 +4,13 @@ let isConnected = false;
 
 export const connectDb = async () => {
   if (isConnected) {
-    console.log(isConnected)
+    // console.log(isConnected)
     console.log('Database is already connected');
     return;
   }
   
   try {
-    const db = await mongoose.connect(process.env.uri || "");
+    const db = await mongoose.connect(process.env.URI || "mongodb+srv://ola:admin@cluster0.bbiilar.mongodb.net/properties?retryWrites=true&w=majority");
     
     isConnected = db.connections[0].readyState === 1;
     
