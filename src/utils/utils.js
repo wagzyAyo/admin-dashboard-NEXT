07c517a1f333fa352c1a7da2b73967bc27124745
Ayo <talktojmcvibes@gmail.com>
2024-12-05 12:39:25 +0100

    create checkAuth function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -8,3 +8,15 @@ export const getData = async (endPoint, stateFunction)=>{
       console.log("Error getting data",err)
     }
   }
+
+  export const checkAuth = async ()=>{
+    try {
+      const response = await fetch('/api/check-auth', {credentials: 'include'})
+      if(response.ok){
+        const data = await response.json();
+        console.log('User Authenticated', data.user)
+      }
+    } catch (err) {
+      console.log('Error check Authentication', err)
+    }
+  }
