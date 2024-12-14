62697832ac59fde2078e5e64407c27e125864490
Ayo <talktojmcvibes@gmail.com>
2024-12-14 19:49:42 +0100

    remove token validation on request data

--- src/app/api/recent/route.ts
+++ src/app/api/recent/route.ts
@@ -3,12 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 import propertyModels from "@/models/props";
 
 export async function GET(req: NextRequest){
-    const token = req.cookies.get('jwt')?.value;
+    // const token = req.cookies.get('jwt')?.value;
 
 
-    if(!token){
-        return NextResponse.json({message: "Unauthorized access"}, {status: 400})
-    }
+    // if(!token){
+    //     return NextResponse.json({message: "Unauthorized access"}, {status: 400})
+    // }
 
     try {
         await connectDb();
